07b161ce0c27c6552f088f0519cd54b35c16ee1a
himanushi <himanushi@users.noreply.github.com>
2020-04-19 00:18:14 +0900

    no image 対応

--- src/components/image/ImageComponent.tsx
+++ src/components/image/ImageComponent.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import CardMedia from '@material-ui/core/CardMedia';
-import Skeleton from '@material-ui/lab/Skeleton';
 
 const ImageComponent = ({
   className = "", src = "", title = "", width
 }:{
   className?:string, src?:string, title?:string, width:string|number
 }) => {
+  if(src === "") src = `${process.env.PUBLIC_URL}/no_image.png`
+
   return (
     <CardMedia
       className={className}
