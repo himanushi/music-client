fbf2cacfbf08435714ab2e279ca8e23262c5111c
himanushi <himanushi@users.noreply.github.com>
2020-04-28 21:07:00 +0900

    Apollo client エラーハンドリング追加

--- src/layouts/app/client.ts
+++ src/layouts/app/client.ts
@@ -2,13 +2,22 @@ import { ApolloClient } from 'apollo-client'
 import { ApolloLink } from 'apollo-link'
 import { HttpLink } from 'apollo-link-http'
 import { InMemoryCache } from 'apollo-cache-inmemory'
+import { onError } from "apollo-link-error"
 
-const headersLink = new ApolloLink((operation, forward) => {
-  return forward(operation)
-})
-
+const headersLink = new ApolloLink((operation, forward) => forward(operation))
 const httpLink = new HttpLink({ uri: `http://localhost:3000/graphql`, credentials : 'include' })
-const link = ApolloLink.from([headersLink, httpLink])
+const errorLink = onError(({ graphQLErrors, networkError }) => {
+  if (graphQLErrors)
+    graphQLErrors.map(({ message, locations, path }) =>
+      console.log(
+        `[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`,
+      ),
+    );
+
+  if (networkError) console.log(`[Network error]: ${networkError}`);
+});
+
+const link = ApolloLink.from([headersLink, httpLink, errorLink])
 
 export default new ApolloClient({
   link,
