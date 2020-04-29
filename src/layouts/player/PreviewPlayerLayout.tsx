9a3d026506cfd2752984f03a849fd750dc2f04b1
himanushi <himanushi@users.noreply.github.com>
2020-04-29 16:56:04 +0900

    Preview Playwr 側追加

---
+++ src/layouts/player/PreviewPlayerLayout.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Grid } from '@material-ui/core';
+
+const PreviewPlayerLayout = () => {
+  return (
+    <Grid
+      container
+      spacing={1}
+      direction="row"
+      justify="center"
+      alignItems="center"
+    >
+      aaaa
+    </Grid>
+  )
+}
+
+export default PreviewPlayerLayout
