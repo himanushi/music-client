9eb2a5c5e0d7799c2a7de0a39b592c81a5bdcfeb
himanushi <himanushi@users.noreply.github.com>
2020-06-28 04:28:14 +0900

    タイトル自動変更を削除していたので修正

--- src/layouts/album/info/AlbumInfoLayout.tsx
+++ src/layouts/album/info/AlbumInfoLayout.tsx
@@ -16,6 +16,11 @@ const AlbumInfoLayout = () => {
   let content = <></>
 
   if(!loading && data && data.album) {
+
+    // SEO対策
+    const titles = document.title.split("-")
+    document.title = `${data.album.name} - ${titles[titles.length - 1].trim()}`
+
     const album_content = <PreviewPlayerComponent album={data.album as Album} />
 
     content =
