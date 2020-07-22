d189f94ed580579e1bfd88491ba66e8f491b3b41
himanushi <himanushi@users.noreply.github.com>
2020-07-23 08:08:41 +0900

    Facebook 共有ボタン追加

--- src/components/player/ShareButtonComponent.tsx
+++ src/components/player/ShareButtonComponent.tsx
@@ -6,12 +6,16 @@ import TwitterIcon from '@material-ui/icons/Twitter'
 import FacebookIcon from '@material-ui/icons/Facebook'
 
 const ShareButtonComponent = ({ album }:{ album:Album }) => {
+  const url = `https://video-game-music.net/albums/${album.id}?ai=${album.id}`
   // TODO: 他のSNSでも汎用的に使えるようにリファクタしろよな
   let twitterUrl = `https://twitter.com/intent/tweet`
   twitterUrl += `?text=${album.name} - ゲーム音楽`
-  twitterUrl += `&url=https://video-game-music.net/albums/${album.id}?ai=${album.id}`
+  twitterUrl += `&url=${url}`
   twitterUrl += `&hashtags=ゲーム音楽,vgm`
   twitterUrl += `&via=vgm_net`
+
+  let facebookUrl = `https://www.facebook.com/sharer/sharer.php`
+  facebookUrl += `?u=${encodeURIComponent(url)}`
 
   return (
     <Grid container direction="row" justify="center" alignItems="center" spacing={2}>
@@ -29,6 +33,16 @@ const ShareButtonComponent = ({ album }:{ album:Album }) => {
           target="_blank"
         >
           <TwitterIcon  />
+        </IconButton>
+      </Grid>
+      <Grid item>
+        {/* ref: https://developers.facebook.com/docs/plugins/share-button?locale=ja_JP#configurator */}
+        <IconButton
+          size="small"
+          href={facebookUrl}
+          target="_blank"
+        >
+          <FacebookIcon  />
         </IconButton>
       </Grid>
     </Grid>
