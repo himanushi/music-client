4339df09b8e28f7c6e7a4ef30c0f96dfa2a0e0fe
himanushi <himanushi@users.noreply.github.com>
2020-09-14 17:18:21 +0900

    about を修正

--- src/layouts/information/AboutLayout.tsx
+++ src/layouts/information/AboutLayout.tsx
@@ -1,15 +1,20 @@
-import React, { useEffect } from 'react';
 import InformationLayout from './InformationLayout'
 
 const AboutLayout = () => InformationLayout(`
 # このサイトについて
+
 ## 概要
 音楽サブスクリプション配信中のゲーム音楽を検索できるサイトです。非商用で公開しています。
+
 ## 目的
 私が新しいゲーム音楽に出会うために公開しています。今後、お気に入り共有機能やプレイリスト機能などの実装により、ユーザー同士でゲーム音楽を共有できるよう開発を進めています。
+
 ## SNSと開発プロジェクト
-[ツイッターはこちら](https://twitter.com/vgm_net)
-[開発プロジェクトはこちら](https://github.com/users/himanushi/projects/2)
+[Twitter](https://twitter.com/vgm_net)
+[Github](https://github.com/users/himanushi/projects/2)
+
+## 問い合わせ
+[Twitter にてダイレクトメッセージをお願いします](https://twitter.com/vgm_net)
 `)
 
 export default AboutLayout
