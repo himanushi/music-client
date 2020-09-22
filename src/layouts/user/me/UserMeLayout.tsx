b1277a24931ccf8417b7a579ea0ecdb5de3b1cee
himanushi <himanushi@users.noreply.github.com>
2020-09-23 07:12:47 +0900

    ユーザー情報画面修正

--- src/layouts/user/me/UserMeLayout.tsx
+++ src/layouts/user/me/UserMeLayout.tsx
@@ -1,100 +1,63 @@
 import React, { useState, useContext, useEffect } from 'react';
-import { Grid, FormControl, InputLabel, Input, Button } from '@material-ui/core';
+import { Grid, FormControl, InputLabel, Input, Button, Card, CardContent, TableContainer, Paper, Table, TableBody, TableRow, TableCell, TableHead } from '@material-ui/core';
 import Alert from '@material-ui/lab/Alert'
-import { useMeQuery, useUpdateMeMutation, UpdateMePayload, UpdateMeInput } from '../../../graphql/types.d';
+import { useMeQuery, useUpdateMeMutation, UpdateMePayload, UpdateMeInput, CurrentUser } from '../../../graphql/types.d';
 import UserContext from '../../../hooks/userContext';
+import { Link } from 'react-router-dom';
 
 const UserMeLayout = () => {
-  const [setup, setSetup] = useState(true)
-  const [notification, setNotification] = useState(<></>)
-  const [name, setName] = useState("")
-  const [username, setUsername] = useState("")
-  const [oldPassword, setOldPassword] = useState("")
-  const [newPassword, setNewPassword] = useState("")
-  const [input, setInput] = useState<UpdateMeInput>({ oldPassword })
-  const { state, dispatch } = useContext(UserContext)
-
-  // カレントユーザーデフォルト値
-  useEffect(() => {
-    if(state.user && setup){
-      setSetup(false)
-      setName(state.user.name)
-      setUsername(state.user.username)
-
-      // 詳細情報はログに出しておく
-      if(state.user){
-        console.log({ id: state.user.id })
-        console.log({ role: state.user.role.allowedActions })
-      }
-    }
-  }, [state, setup])
-
-  // カレントユーザー更新
-  interface UpdateMeResponse {
-    data: { updateMe: UpdateMePayload }
-  }
-  const [updateMe] = useUpdateMeMutation({
-    update: (_, response:UpdateMeResponse) => {
-      if (response.data.updateMe.error) {
-        setNotification(<Alert severity="error">{response.data.updateMe.error}</Alert>)
-      } else {
-        setNotification(<Alert severity="success">更新しました</Alert>)
-      }
-    },
-    variables: { input },
-  })
+  const { state } = useContext(UserContext)
 
   return (
-    <Grid
-      container
-      spacing={1}
-      direction="row"
-      justify="center"
-      alignItems="center"
-    >
-      <form autoComplete="off">
-        <div>
-          <FormControl>
-            <InputLabel>名前</InputLabel>
-            <Input value={name} onChange={e => {
-              setName(e.target.value || "")
-              setInput({ ...input, name: (e.target.value || "") })
-            }}/>
-          </FormControl>
-        </div>
-        <div>
-          <FormControl>
-            <InputLabel>ユーザー名</InputLabel>
-            <Input value={username} onChange={e => {
-              setUsername(e.target.value || "")
-              setInput({ ...input, username: (e.target.value || "") })
-            }}/>
-          </FormControl>
-        </div>
-        <div>
-          <FormControl>
-            <InputLabel>新しいパスワード</InputLabel>
-            <Input value={newPassword} onChange={e => {
-              setNewPassword(e.target.value || "")
-              setInput({ ...input, newPassword: (e.target.value || "") })
-            }} type="password" />
-          </FormControl>
-        </div>
-        <div>
-          <FormControl required={true}>
-            <InputLabel>古いパスワード</InputLabel>
-            <Input value={oldPassword} onChange={e => {
-              setOldPassword(e.target.value || "")
-              setInput({ ...input, oldPassword: (e.target.value || "") })
-            }} type="password" />
-          </FormControl>
-        </div>
-        <div>
-          <Button type="submit" onClick={(e) =>{e.preventDefault(); updateMe()}} variant="contained">Update</Button>
-        </div>
-        <div>{notification}</div>
-      </form>
-    </Grid>
+    <TableContainer component={Paper}>
+      <Table>
+        <TableHead>
+          <TableRow>
+            <TableCell colSpan={2} align="center" style={{ border: 'none' }}>ユーザー情報</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          <TableRow >
+            <TableCell align="right" style={{ border: 'none' }}>ユーザー名</TableCell>
+            <TableCell align="left" style={{ border: 'none' }}>{state.user?.name}</TableCell>
+          </TableRow>
+          <TableRow>
+            <TableCell align="right" style={{ border: 'none' }}>ユーザーID</TableCell>
+            <TableCell align="left" style={{ border: 'none' }}>{state.user?.username}</TableCell>
+          </TableRow>
+          <TableRow>
+            <TableCell align="right">権限</TableCell>
+            <TableCell align="left">{state.user?.role.name}</TableCell>
+          </TableRow>
+        </TableBody>
+      </Table>
+      <Table>
+        <TableHead>
+          <TableRow>
+            <TableCell colSpan={2} align="center" style={{ border: 'none' }}>公開情報</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          <TableRow>
+            <TableCell align="right" style={{ border: 'none' }}>好きなアーティスト</TableCell>
+            <TableCell align="left" style={{ border: 'none' }}>公開する</TableCell>
+          </TableRow>
+          <TableRow>
+            <TableCell align="right">好きなアルバム</TableCell>
+            <TableCell align="left">公開する</TableCell>
+          </TableRow>
+        </TableBody>
+      </Table>
+      <Table>
+        <TableHead>
+          <TableRow>
+            <TableCell colSpan={2} align="center">
+              <Button component={Link} to={"/me/edit"} variant="contained">変更する</Button>
+            </TableCell>
+          </TableRow>
+        </TableHead>
+      </Table>
+    </TableContainer>
   )
 }
 
