519717e0a0171b1d147f2e2789542408efaa9593
himanushi <himanushi@users.noreply.github.com>
2020-09-22 17:05:39 +0900

    ログイン画面整えた

--- src/layouts/user/login/UserLoginLayout.tsx
+++ src/layouts/user/login/UserLoginLayout.tsx
@@ -1,12 +1,15 @@
-import React, { useState } from 'react';
-import { Grid, FormControl, InputLabel, Input, Button } from '@material-ui/core';
+import React, { useState, useContext } from 'react';
+import { Grid, FormControl, InputLabel, Input, Button, Card, CardContent } from '@material-ui/core';
 import Alert from '@material-ui/lab/Alert'
-import { useLoginMutation, LoginPayload } from '../../../graphql/types.d';
+import { useLoginMutation, LoginPayload, CurrentUser } from '../../../graphql/types.d';
+import UserContext from '../../../hooks/userContext';
 
 const UserLoginLayout = () => {
   const [notification, setNotification] = useState(<></>)
   const [username, setUsername] = useState("")
   const [password, setPassword] = useState("")
+
+  const { state, dispatch } = useContext(UserContext)
 
   interface LoginResponse {
     data: { login: LoginPayload }
@@ -16,6 +19,7 @@ const UserLoginLayout = () => {
       if (response.data.login.error) {
         setNotification(<Alert severity="error">{response.data.login.error}</Alert>)
       } else {
+        dispatch({ type: "SET_USER", user: response.data.login.currentUser as CurrentUser })
         setNotification(<Alert severity="success">ログインしました</Alert>)
       }
     },
@@ -23,32 +27,36 @@ const UserLoginLayout = () => {
   })
 
   return (
-    <Grid
-      container
-      spacing={1}
-      direction="row"
-      justify="center"
-      alignItems="center"
-    >
-      <form autoComplete="off">
-        <div>
-          <FormControl>
-            <InputLabel>ユーザー名</InputLabel>
-            <Input value={username} onChange={e => setUsername(e.target.value || "")}/>
-          </FormControl>
-        </div>
-        <div>
-          <FormControl>
-            <InputLabel>パスワード</InputLabel>
-            <Input value={password} onChange={e => setPassword(e.target.value || "")} type="password" />
-          </FormControl>
-        </div>
-        <div>
-          <Button type="submit" onClick={(e) =>{e.preventDefault(); login()}} variant="contained">ログイン</Button>
-        </div>
-        <div>{notification}</div>
-      </form>
-    </Grid>
+    <Card>
+      <CardContent>
+        <form autoComplete="off">
+          <Grid
+            container
+            spacing={1}
+            direction="column"
+            justify="center"
+            alignItems="center"
+          >
+            <Grid item>
+              <FormControl>
+                <InputLabel>ユーザーID</InputLabel>
+                <Input value={username} onChange={e => setUsername(e.target.value || "")}/>
+              </FormControl>
+            </Grid>
+            <Grid item>
+              <FormControl>
+                <InputLabel>パスワード</InputLabel>
+                <Input value={password} onChange={e => setPassword(e.target.value || "")} type="password" />
+              </FormControl>
+            </Grid>
+            <Grid item>
+              <Button type="submit" onClick={(e) =>{e.preventDefault(); login()}} variant="contained">ログイン</Button>
+            </Grid>
+            <Grid item>{notification}</Grid>
+          </Grid>
+        </form>
+      </CardContent>
+    </Card>
   )
 }
 
