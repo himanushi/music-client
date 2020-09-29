d140806a64fa48cf0ecf53679e535f74af0bfe0a
himanushi <himanushi@users.noreply.github.com>
2020-09-30 07:21:55 +0900

    ログインスナックバー対応

--- src/layouts/user/login/UserLoginLayout.tsx
+++ src/layouts/user/login/UserLoginLayout.tsx
@@ -1,15 +1,18 @@
 import React, { useState, useContext } from 'react';
-import { Grid, FormControl, InputLabel, Input, Button, Card, CardContent } from '@material-ui/core';
-import Alert from '@material-ui/lab/Alert'
+import { FormControl, InputLabel, Input, Button, TableContainer, Paper, Table, TableHead, TableRow, TableCell, TableBody } from '@material-ui/core';
 import { useLoginMutation, LoginPayload, CurrentUser } from '../../../graphql/types.d';
 import UserContext from '../../../hooks/userContext';
+import InformationContext from '../../../hooks/informationContext';
+import { useHistory } from 'react-router-dom';
 
 const UserLoginLayout = () => {
-  const [notification, setNotification] = useState(<></>)
   const [username, setUsername] = useState("")
   const [password, setPassword] = useState("")
 
-  const { state, dispatch } = useContext(UserContext)
+  const userContext = useContext(UserContext)
+  const infoContext = useContext(InformationContext)
+
+  let history = useHistory()
 
   interface LoginResponse {
     data: { login: LoginPayload }
@@ -17,46 +20,49 @@ const UserLoginLayout = () => {
   const [login] = useLoginMutation({
     update: (_, response:LoginResponse) => {
       if (response.data.login.error) {
-        setNotification(<Alert severity="error">{response.data.login.error}</Alert>)
+        infoContext.dispatch({ type: "ADD_ALERT", severity: "error", duration: 5000, text: response.data.login.error, buttonText: "OK" })
       } else {
-        dispatch({ type: "SET_USER", user: response.data.login.currentUser as CurrentUser })
-        setNotification(<Alert severity="success">ログインしました</Alert>)
+        userContext.dispatch({ type: "SET_USER", user: response.data.login.currentUser as CurrentUser })
+        infoContext.dispatch({ type: "ADD_ALERT", severity: "success", duration: 5000, text: "ログインしました。", buttonText: "OK" })
+        history.push("/albums")
       }
     },
     variables: { input: { username, password } },
   })
 
   return (
-    <Card>
-      <CardContent>
-        <form autoComplete="off">
-          <Grid
-            container
-            spacing={1}
-            direction="column"
-            justify="center"
-            alignItems="center"
-          >
-            <Grid item>
-              <FormControl>
+    <TableContainer component={Paper}>
+      <Table>
+        <TableHead>
+          <TableRow>
+            <TableCell align="center" style={{ border: 'none' }}>ログイン</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          <TableRow >
+            <TableCell align="center" style={{ border: 'none' }}>
+              <FormControl required={true}>
                 <InputLabel>ユーザーID</InputLabel>
                 <Input value={username} onChange={e => setUsername(e.target.value || "")}/>
               </FormControl>
-            </Grid>
-            <Grid item>
-              <FormControl>
+            </TableCell>
+          </TableRow>
+          <TableRow >
+             <TableCell align="center" style={{ border: 'none' }}>
+              <FormControl required={true}>
                 <InputLabel>パスワード</InputLabel>
                 <Input value={password} onChange={e => setPassword(e.target.value || "")} type="password" />
               </FormControl>
-            </Grid>
-            <Grid item>
-              <Button type="submit" onClick={(e) =>{e.preventDefault(); login()}} variant="contained">ログイン</Button>
-            </Grid>
-            <Grid item>{notification}</Grid>
-          </Grid>
-        </form>
-      </CardContent>
-    </Card>
+            </TableCell>
+          </TableRow>
+          <TableRow>
+            <TableCell align="center">
+              <Button disabled={!username || !password} variant="contained" onClick={() => login()}>ログインする</Button>
+            </TableCell>
+          </TableRow>
+        </TableBody>
+      </Table>
+    </TableContainer>
   )
 }
 
