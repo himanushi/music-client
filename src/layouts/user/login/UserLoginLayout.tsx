93692cd9c7a0b83080b2536b357d23202d17a202
himanushi <himanushi@users.noreply.github.com>
2020-10-10 20:37:40 +0900

    ログイン画面にreCaptcha追加sita

--- src/layouts/user/login/UserLoginLayout.tsx
+++ src/layouts/user/login/UserLoginLayout.tsx
@@ -1,18 +1,29 @@
-import React, { useState, useContext } from 'react';
+import React, { useState, useContext, useEffect, useRef, createRef } from 'react';
 import { FormControl, InputLabel, Input, Button, TableContainer, Paper, Table, TableHead, TableRow, TableCell, TableBody } from '@material-ui/core';
 import { useLoginMutation, LoginPayload, CurrentUser } from '../../../graphql/types.d';
 import UserContext from '../../../hooks/userContext';
 import InformationContext from '../../../hooks/informationContext';
 import { useHistory } from 'react-router-dom';
+import ReCAPTCHA from "react-google-recaptcha";
 
 const UserLoginLayout = () => {
   const [username, setUsername] = useState("")
   const [password, setPassword] = useState("")
+  const [token,    setToken] = useState("")
+  const recaptchaRef = useRef<ReCAPTCHA>() as React.RefObject<ReCAPTCHA>
 
   const userContext = useContext(UserContext)
   const infoContext = useContext(InformationContext)
 
   let history = useHistory()
+
+  useEffect(() => {
+    if(token) {
+      document.cookie = `reCAPTCHAv2Token=${token};`
+    }
+
+    return () => { document.cookie = `reCAPTCHAv2Token=;` }
+  }, [token])
 
   if(userContext.state.user?.registered) {
     infoContext.dispatch({ type: "ADD_ALERT", severity: "info", duration: 5000, text: "ログイン済みです", buttonText: "OK" })
@@ -25,6 +36,7 @@ const UserLoginLayout = () => {
   const [login] = useLoginMutation({
     update: (_, response:LoginResponse) => {
       if (response.data.login.error) {
+        recaptchaRef?.current?.reset()
         infoContext.dispatch({ type: "ADD_ALERT", severity: "error", duration: 5000, text: response.data.login.error, buttonText: "OK" })
       } else {
         userContext.dispatch({ type: "SET_USER", user: response.data.login.currentUser as CurrentUser })
@@ -60,9 +72,18 @@ const UserLoginLayout = () => {
               </FormControl>
             </TableCell>
           </TableRow>
+          <TableRow >
+            <TableCell align="center" style={{ border: 'none' }}>
+              <ReCAPTCHA
+                ref={recaptchaRef}
+                sitekey={process.env.REACT_APP_RECAPTCHA_KEY || ""}
+                onChange={_token => setToken(_token as string)}
+              />
+            </TableCell>
+          </TableRow>
           <TableRow>
             <TableCell align="center">
-              <Button disabled={!username || !password} variant="contained" onClick={() => login()}>ログインする</Button>
+              <Button disabled={!username || !password || !token} variant="contained" onClick={() => login()}>ログインする</Button>
             </TableCell>
           </TableRow>
         </TableBody>
